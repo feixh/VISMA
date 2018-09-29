7b2f898c3e7bf7f80ff6aed0c87293d07c149d3e
feixh <hzhsfxh@gmail.com>
2018-09-28 18:09:18 -0700

    cannot concatenate images ...

--- example/example_dump.cpp
+++ example/example_dump.cpp
@@ -6,10 +6,13 @@
 
 // 3rdparty
 #include "folly/Format.h"
+#include "opencv2/imgproc.hpp"
 
 // feh
 #include "dataloader.h"
 #include "utils.h"
+
+const int downsample_rate = 2;
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -62,42 +65,44 @@ int main(int argc, char *argv[]) {
         std::cout << each << " ";
     }
 
+    cv::Mat prev_img, next_img;
     for (int i = 0; i < loader->size(); ++i) {
+        if (i == 0 || i+1 == loader->size()) continue;
         cv::Mat img, edgemap;   // image and edge map
         vlslam_pb::BoundingBoxList bboxlist;    // list of bounding boxes
-        Sophus::SE3f gwc;   // camera to world transformation
+        Sophus::SE3f gwc, gwc1, gwc2;   // camera to world transformation
         Sophus::SO3f Rg;    // rotation to align with gravity
+
         loader->Grab(i, img, edgemap, bboxlist, gwc, Rg);   // grab datum
+        loader->Grab(i-1, prev_img, edgemap, bboxlist, gwc1, Rg);   // grab datum
+        loader->Grab(i+1, next_img, edgemap, bboxlist, gwc2, Rg);   // grab datum
         auto depth_samples = loader->GrabSparseDepth(i);
+
+        int rows = img.rows / 2, cols = img.cols / 2;
+        cv::Mat img_down{rows, cols, CV_8UC3}; 
+        cv::Mat prev_img_down{rows, cols, CV_8UC3};
+        cv::Mat next_img_down{rows, cols, CV_8UC3};
+        cv::pyrDown(img, img_down);
+        cv::pyrDown(prev_img, prev_img_down);
+        cv::pyrDown(next_img, next_img_down);
+
+        cv::Mat seq{rows, cols * 3, CV_8UC3};
+        img_down.copyTo(seq(cv::Rect(cols, 0, cols, rows)));
+        prev_img_down.copyTo(seq(cv::Rect(0, 0, cols, rows)));
+        next_img_down.copyTo(seq(cv::Rect(cols*2, 0, cols, rows)));
 
         std::cout << "g(world <- camera)=\n" << gwc.matrix3x4() << std::endl;
         std::cout << "Rg=\n" << Rg.matrix() << std::endl;
 
-        // print bounding boxes
-        int j = 0;
-        for (const auto &bbox : bboxlist.bounding_boxes()) {
-            std::cout << "bbox #" << j++
-                      << "(" << bbox.top_left_x() << "," << bbox.top_left_y() << ")-"
-                      << "(" << bbox.bottom_right_x() << "," << bbox.bottom_right_y() << ")\n"
-                      << "class=" << bbox.class_name() << "\n"
-                      << "scores=";
-            // Scores can store a list of scores of all the classes.
-            // However, we only keep the score of the most likely class at index 0.
-            for (const auto &score : bbox.scores()) std::cout << score << " ";
-            std::cout << std::endl;
-        }
-        cv::imshow("image", img);
-        cv::imshow("edge map", edgemap);
-        cv::waitKey(30);
-
-        // write out image
-        cv::imwrite(folly::sformat("{}/image/{:06d}.jpg", argv[2], i), img);
-
         // write out pose
         std::ofstream fid_pose;
         try {
+            auto g1c = gwc1.inverse() * gwc;     // current to previous one
+            auto g2c = gwc2.inverse() * gwc;     // current to next one
             fid_pose.open(folly::sformat("{}/pose/{:06d}.txt", argv[2], i));
-            fid_pose << gwc.matrix3x4();
+            fid_pose << g1c.matrix3x4();
+            fid_pose << std::endl;
+            fid_pose << g2c.matrix3x4();
             fid_pose.close();
         } catch (const std::exception &) {
             exit(-1);
@@ -108,12 +113,20 @@ int main(int argc, char *argv[]) {
         try {
             fid_depth.open(folly::sformat("{}/depth/{:06d}.txt", argv[2], i));
             for (const auto &s : depth_samples) {
-                fid_depth << s.second[0] << " " << s.second[1] << " " << s.second[2] << std::endl;
+                fid_depth << s.second[0] / downsample_rate << " " 
+                          << s.second[1] / downsample_rate << " " 
+                          << s.second[2] << std::endl;
             }
             fid_depth.close();
         } catch (const std::exception &) {
             exit(-1);
         }
+
+        cv::imshow("image", img_down);
+        // cv::imshow("edge map", edgemap);
+        cv::waitKey(30);
+        // write out image
+        cv::imwrite(folly::sformat("{}/image/{:06d}.jpg", argv[2], i), seq);
 
     }
 }
